7747dc96dd409a8143d196270b5688379d41e871
Anastasiia Utina <anastasiiaprekrasnaiia@gmail.com>
2022-11-09 15:32:47 +0300

    add files

---
+++ src/main/java/ru/netology/product/Product.java
@@ -0,0 +1,35 @@
+package ru.netology.product;
+
+public class Product {
+    protected int ID;
+    protected String name;
+    protected int price;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package ru.netology.repository;
+
+
+public class ProductRepository {
+    private Product[] getProduct;
+
+    public  void save(Product product){
+        ///
+    }
+    public  void removeId(int id){
+        //
+    }
+    public Product[] getProduct(){
+
+    }
+}
+
